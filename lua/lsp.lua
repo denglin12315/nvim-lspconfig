02a058d63ed8682ab0c26db64f769e876347a082
lin.deng <denglin12315@163.com>
2026-08-06 11:24:02 +0800

    adapt lsp update by NVIM-0.12
    Signed-off-by: lin.deng <denglin12315@163.com>

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,38 +1,53 @@
--- Set different settings for different languages' LSP
--- LSP list: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
--- How to use setup({}): https://github.com/neovim/nvim-lspconfig/wiki/Understanding-setup-%7B%7D
---     - the settings table is sent to the LSP
---     - on_attach: a lua callback function to run after LSP attaches to a given buffer
-local lspconfig = require("lspconfig")
+-- Using vim.lsp.config() with separate config calls
 
--- Customized on_attach function
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap = true, silent = true }
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+local function on_attach(client, bufnr)
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
 
-	vim.keymap.set("n", "gc", vim.lsp.buf.declaration, bufopts)
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-	vim.keymap.set("n", "gh", vim.lsp.buf.hover, bufopts)
-	vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, bufopts)
-	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-    -- for function call:wqa
-	vim.keymap.set("n", "ci", vim.lsp.buf.incoming_calls, bufopts)
-	vim.keymap.set("n", "co", vim.lsp.buf.outgoing_calls, bufopts)
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+    vim.keymap.set("n", "<leader>ci", vim.lsp.buf.incoming_calls, bufopts)
+    vim.keymap.set("n", "<leader>co", vim.lsp.buf.outgoing_calls, bufopts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set("n", "<leader>f", function()
+        vim.lsp.buf.format { async = true }
+    end, bufopts)
 end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-lspconfig.clangd.setup({
-	on_attach = on_attach,
-    capabilities=capabilities,
+-- Configure each server individually
+vim.lsp.config("clangd", {
+    cmd = { "clangd" },
+    filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+    root_markers = { ".clangd", "compile_commands.json", "compile_flags.txt", ".git" },
+    on_attach = on_attach,
+    capabilities = capabilities,
 })
 
-lspconfig.gopls.setup({
-	on_attach = on_attach,
-    capabilities=capabilities,
+vim.lsp.config("gopls", {
+    cmd = { "gopls" },
+    filetypes = { "go", "gomod", "gowork", "gotmpl" },
+    root_markers = { "go.mod", "go.work", ".git" },
+    on_attach = on_attach,
+    capabilities = capabilities,
+    settings = {
+        gopls = {
+            analyses = {
+                unusedparams = true,
+                shadow = true,
+            },
+            staticcheck = true,
+            usePlaceholders = true,
+            completeUnimported = true,
+        },
+    },
 })
+
+-- Enable the servers
+vim.lsp.enable("clangd", "gopls")
+
